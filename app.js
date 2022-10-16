6ae6eda4ae4b70a23f5f70622daa1acf3d26a510
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-10-15 23:53:30 -0700

    Changed MongoDB URI.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var helmet = require("helmet");
 var app = express();
 
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://ntngu:<password>@cluster0.ip4e797.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://ntngu:<password>@cluster0.ip4e797.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));

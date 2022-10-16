57123a83cefc81df15155cb61a04727e8cf25035
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-10-15 23:51:07 -0700

    Added more dependencies.

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require("./routes/catalog");
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
 
 const mongoose = require("mongoose");
@@ -24,6 +27,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
